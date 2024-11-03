fe493ed0c7b5d1eb4f2a534d83703a9fd2aa73bb
Rana Elsayed Elshirbeny <150361241+ranaelshirbeny@users.noreply.github.com>
2024-11-03 21:13:20 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace task3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ADD = new System.Windows.Forms.Button();
+            this.REMOVE = new System.Windows.Forms.Button();
+            this.CLEAR = new System.Windows.Forms.Button();
+            this.EXIT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(33, 146);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 292);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(478, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ADD
+            // 
+            this.ADD.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ADD.Location = new System.Drawing.Point(562, 174);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(75, 23);
+            this.ADD.TabIndex = 2;
+            this.ADD.Text = "Add";
+            this.ADD.UseVisualStyleBackColor = false;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // REMOVE
+            // 
+            this.REMOVE.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.REMOVE.Location = new System.Drawing.Point(562, 222);
+            this.REMOVE.Name = "REMOVE";
+            this.REMOVE.Size = new System.Drawing.Size(75, 23);
+            this.REMOVE.TabIndex = 3;
+            this.REMOVE.Text = "Remove";
+            this.REMOVE.UseVisualStyleBackColor = false;
+            this.REMOVE.Click += new System.EventHandler(this.REMOVE_Click);
+            // 
+            // CLEAR
+            // 
+            this.CLEAR.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CLEAR.Location = new System.Drawing.Point(562, 270);
+            this.CLEAR.Name = "CLEAR";
+            this.CLEAR.Size = new System.Drawing.Size(75, 23);
+            this.CLEAR.TabIndex = 4;
+            this.CLEAR.Text = "Clear";
+            this.CLEAR.UseVisualStyleBackColor = false;
+            this.CLEAR.Click += new System.EventHandler(this.CLEAR_Click);
+            // 
+            // EXIT
+            // 
+            this.EXIT.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.EXIT.Location = new System.Drawing.Point(562, 314);
+            this.EXIT.Name = "EXIT";
+            this.EXIT.Size = new System.Drawing.Size(75, 23);
+            this.EXIT.TabIndex = 5;
+            this.EXIT.Text = "Exit";
+            this.EXIT.UseVisualStyleBackColor = false;
+            this.EXIT.Click += new System.EventHandler(this.EXIT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EXIT);
+            this.Controls.Add(this.CLEAR);
+            this.Controls.Add(this.REMOVE);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.Button REMOVE;
+        private System.Windows.Forms.Button CLEAR;
+        private System.Windows.Forms.Button EXIT;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace task3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+
+        }
+
+        private void REMOVE_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void CLEAR_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void EXIT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
